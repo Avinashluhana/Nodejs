cdb57db6ee35be80902f426d00ae5f0f06d095bd
Avinashluhana <aviluhana@gmail.com>
2021-08-03 14:17:18 +0500

    Commited

---
+++ async.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+// fs.mkdir("Async", (err) => {
+//     console.log("Folder has been creaded");
+
+// });
+
+// fs.writeFile('./Async/async.txt','Async file has been creaated', (err) => {
+//     console.log("file created susccesfully");
+
+// });
+
+fs.appendFile("./Async/async.txt", "text appended", (err) => {
+    console.log("appended")
+})
+
+// fs.readFile("./Async/async.txt","utf-8", (err, data) => {
+//     console.log(data);
+
+// });
+
